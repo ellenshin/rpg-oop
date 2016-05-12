c80167ac0bca7defa035530f0698946c560f2bcd
ellenshin <ellenshin1001@gmail.com>
2016-05-12 12:45:12 -0700

    This is the first commit

---
+++ rpgoop/Kimara.swift
@@ -0,0 +1,31 @@
+//
+//  Kimara.swift
+//  rpgoop
+//
+//  Created by Ellen Shin on 5/12/16.
+//  Copyright © 2016 Ellen Shin. All rights reserved.
+//
+
+import Foundation
+
+class Kimara: Enemy {
+    
+    let IMMUNE_MAX = 15
+    
+    override var loot: [String] {
+        return ["Tough Hide", "Kimara Venom", "Rare Triton"]
+    }
+    
+    override var type: String {
+        return "Kimara"
+    }
+    
+    override func attemptAttack(attackPwr: Int) -> Bool {
+        if attackPwr >= IMMUNE_MAX {
+            return super.attemptAttack(attackPwr)
+        } else {
+            return false
+        }
+    }
+    
+}
--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -10,16 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLbl: UILabel!
+    @IBOutlet weak var playerHpLbl: UILabel!
+    @IBOutlet weak var enemyHpLbl: UILabel!
+    @IBOutlet weak var enemyImg: UIImageView!
+    @IBOutlet weak var chestBtn: UIButton!
+
+    
+    var player: Player!
+    var enemy: Enemy!
+    var chestMsg: String?
     override func viewDidLoad() {
+        player = Player(name: "DirtyLaundry", hp: 110, attackPwr: 20)
+        playerHpLbl.text = "\(player.hp) HP"
+        generateRandEnemy()
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func generateRandEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
+        enemyImg.hidden = false
+    }
+    @IBAction func onChestTap(sender: AnyObject) {
+        chestBtn.hidden = true
+        printLbl.text = chestMsg
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(ViewController.generateRandEnemy), userInfo: nil, repeats: false)
+        
     }
 
+    @IBAction func attackTapped(sender: AnyObject) {
+        if enemy.attemptAttack(player.attackPwr) {
+            printLbl.text = "Attacked \(enemy.type) for \(player.attackPwr) HP"
+            enemyHpLbl.text = "\(enemy.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful!"
+        }
+        
+        if let loot = enemy.dropLoot() {
+            chestMsg = "\(player.name) found \(loot)"
+            player.addItemToInventory(loot)
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+        }
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
 
